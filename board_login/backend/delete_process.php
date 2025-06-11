6f7ba81a8e399e0e42375676ac361247da166914
HSeung03 <lsh232986@naver.com>
2025-06-11 22:57:12 +0900

    added comments in delete_process

--- board_login/backend/delete_process.php
+++ board_login/backend/delete_process.php
@@ -3,15 +3,16 @@ $conn = new mysqli("mysql", "root", "12345678", "board_login");
 if ($conn->connect_error) die("DB 연결 실패");
 $conn->set_charset("utf8mb4");
 
-#POST로 받은 ID
-$id = isset($_POST['id']) ? (int)$_POST['id'] : 0;
+
+$id = (int)($_POST['id'] ?? 0);
 
 if ($id === 0) {
     die("잘못된 접근입니다.");
 }
 
-#게시글 삭제
+#board에 있는 특정 아이디의 데이터를 삭제
 $stmt = $conn->prepare("DELETE FROM board WHERE id = ?");
+#보안성 처리
 $stmt->bind_param("i", $id);
 
 if ($stmt->execute()) {
